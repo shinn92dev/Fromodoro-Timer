f74bdb5b5ac0e6789b5932779207b3eea20c5622
Anthony Shin <shinn92dev@gmail.com>
2024-06-18 13:49:40 -0700

    :wrench:conf: add Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import "./input.css";
+
 function App() {
     return (
         <div className="App">
-            <h1>Fromodoro Timer</h1>
+            <h1 className="">Fromodoro Timer</h1>
+            <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>
         </div>
     );
 }
